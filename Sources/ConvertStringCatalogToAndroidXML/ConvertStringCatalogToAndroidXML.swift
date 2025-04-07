bde7fb5c90baa8096fdd84e3d52e1457dc8b0d4f
msuess <moritz.suess@hof-university.de>
2025-04-07 10:10:28 +0200

    escaping new lines and tabs

--- Sources/ConvertStringCatalogToAndroidXML/ConvertStringCatalogToAndroidXML.swift
+++ Sources/ConvertStringCatalogToAndroidXML/ConvertStringCatalogToAndroidXML.swift
@@ -14,32 +14,36 @@ struct ConvertStringCatalogToAndroidXML: ParsableCommand {
   @Option(help: "Output path for the generated Android XML file")
   public var outputPath: String
 
-  public func run() throws {
-    guard let catalog = StringCatalog(contentsOfFile: xcstringsPath) else {
-      print("Could not parse file at \(xcstringsPath)")
-      throw ExitCode.failure
-    }
-      
-      let baseLanguage = StringLanguage(rawValue: baseLanguage)
-
-      for outputLanguage in catalog.languages {
-          let xmlDocument = catalog.converted(to: outputLanguage)
-
-          let url = (outputLanguage == baseLanguage)
-          ? URL(fileURLWithPath: outputPath + "/values/")
-          : URL(fileURLWithPath: outputPath + "/values-\(outputLanguage.rawValue)/")
-
-          try! FileManager.default.createDirectory(
+    public func run() throws {
+        guard let catalog = StringCatalog(contentsOfFile: xcstringsPath) else {
+            print("Could not parse file at \(xcstringsPath)")
+            throw ExitCode.failure
+        }
+        
+        let baseLanguage = StringLanguage(rawValue: baseLanguage)
+        
+        for outputLanguage in catalog.languages {
+            let xmlDocument = catalog.converted(to: outputLanguage)
+            
+            var finalXmlString = xmlDocument.prettyPrinted
+            
+            finalXmlString = finalXmlString.replacingOccurrences(of: "&amp;#", with: "&#")
+            
+            let url = (outputLanguage == baseLanguage)
+            ? URL(fileURLWithPath: outputPath + "/values/")
+            : URL(fileURLWithPath: outputPath + "/values-\(outputLanguage.rawValue)/")
+            
+            try! FileManager.default.createDirectory(
                 at: url,
                 withIntermediateDirectories: true
-              )
-
-          try! xmlDocument.prettyPrinted
-            .write(
-              toFile: url.path() + "/strings.xml",
-              atomically: true,
-              encoding: .utf8
             )
-      }
-  }
+            
+            try! finalXmlString
+                .write(
+                    toFile: url.path() + "/strings.xml",
+                    atomically: true,
+                    encoding: .utf8
+                )
+        }
+    }
 }
