7cd7d1a8d1770dfb308d096c2caa502afb73acfa
msuess <moritz.suess@hof-university.de>
2025-04-07 16:39:43 +0200

    formatting

--- Sources/ConvertStringCatalogToAndroidXML/ConvertStringCatalogToAndroidXML.swift
+++ Sources/ConvertStringCatalogToAndroidXML/ConvertStringCatalogToAndroidXML.swift
@@ -7,43 +7,43 @@ import StringCatalogConverterLibrary
 struct ConvertStringCatalogToAndroidXML: ParsableCommand {
   @Option(help: "Specify the path to the xcstrings file")
   public var xcstringsPath: String
-
+  
   @Option(help: "Base language")
   public var baseLanguage: String
-
+  
   @Option(help: "Output path for the generated Android XML file")
   public var outputPath: String
-
-    public func run() throws {
-        guard let catalog = StringCatalog(contentsOfFile: xcstringsPath) else {
-            print("Could not parse file at \(xcstringsPath)")
-            throw ExitCode.failure
-        }
-        
-        let baseLanguage = StringLanguage(rawValue: baseLanguage)
-        
-        for outputLanguage in catalog.languages {
-            let xmlDocument = catalog.converted(to: outputLanguage)
-            
-            var finalXmlString = xmlDocument.prettyPrinted
-            
-            finalXmlString = finalXmlString.replacingOccurrences(of: "&amp;#", with: "&#")
-            
-            let url = (outputLanguage == baseLanguage)
-            ? URL(fileURLWithPath: outputPath + "/values/")
-            : URL(fileURLWithPath: outputPath + "/values-\(outputLanguage.rawValue)/")
-            
-            try! FileManager.default.createDirectory(
-                at: url,
-                withIntermediateDirectories: true
-            )
-            
-            try! finalXmlString
-                .write(
-                    toFile: url.path() + "/strings.xml",
-                    atomically: true,
-                    encoding: .utf8
-                )
-        }
+  
+  public func run() throws {
+    guard let catalog = StringCatalog(contentsOfFile: xcstringsPath) else {
+      print("Could not parse file at \(xcstringsPath)")
+      throw ExitCode.failure
     }
+    
+    let baseLanguage = StringLanguage(rawValue: baseLanguage)
+    
+    for outputLanguage in catalog.languages {
+      let xmlDocument = catalog.converted(to: outputLanguage)
+      
+      var finalXmlString = xmlDocument.prettyPrinted
+      
+      finalXmlString = finalXmlString.replacingOccurrences(of: "&amp;#", with: "&#")
+      
+      let url = (outputLanguage == baseLanguage)
+      ? URL(fileURLWithPath: outputPath + "/values/")
+      : URL(fileURLWithPath: outputPath + "/values-\(outputLanguage.rawValue)/")
+      
+      try! FileManager.default.createDirectory(
+        at: url,
+        withIntermediateDirectories: true
+      )
+      
+      try! finalXmlString
+        .write(
+          toFile: url.path() + "/strings.xml",
+          atomically: true,
+          encoding: .utf8
+        )
+    }
+  }
 }
--- Sources/StringCatalogConverterLibrary/StringCatalog+Conversion.swift
+++ Sources/StringCatalogConverterLibrary/StringCatalog+Conversion.swift
@@ -3,55 +3,55 @@ import StringCatalog
 
 extension StringCatalog {
   static func cleanupKeyForAndroid(_ key: String) -> String {
-      key
-          .replacingOccurrences(of: ".", with: "_")
-          .replacingOccurrences(of: "-", with: "_")
+    key
+      .replacingOccurrences(of: ".", with: "_")
+      .replacingOccurrences(of: "-", with: "_")
   }
-    
-    static func cleanupValueForAndroid(_ value: String) -> String {
-        value
-          .replacingOccurrences(of: "\n", with: "\\n")
-          .replacingOccurrences(of: "\t", with: "\\t")
-          .replacingOccurrences(of: "'", with: "\\'")
-          .replacingOccurrences(of: "%@", with: "%1$s")
-          .replacingOccurrences(of: "%d", with: "%2$d")
-          //.replacingOccurrences(of: "%%", with: "&#x0025;")
-          //.replacingOccurrences(of: "«", with: "&#x00AB;")
-          //.replacingOccurrences(of: "»", with: "&#x00BB;")
-          //.replacingOccurrences(of: "á", with: "&#x00E1;")
-          //.replacingOccurrences(of: "é", with: "&#x00E9;")
-    }
-    
+  
+  static func cleanupValueForAndroid(_ value: String) -> String {
+    value
+      .replacingOccurrences(of: "\n", with: "\\n")
+      .replacingOccurrences(of: "\t", with: "\\t")
+      .replacingOccurrences(of: "'", with: "\\'")
+      .replacingOccurrences(of: "%@", with: "%1$s")
+      .replacingOccurrences(of: "%d", with: "%2$d")
+    //.replacingOccurrences(of: "%%", with: "&#x0025;")
+    //.replacingOccurrences(of: "«", with: "&#x00AB;")
+    //.replacingOccurrences(of: "»", with: "&#x00BB;")
+    //.replacingOccurrences(of: "á", with: "&#x00E1;")
+    //.replacingOccurrences(of: "é", with: "&#x00E9;")
+  }
+  
   public func converted(
     to language: StringLanguage
   ) -> XMLDocument {
-      let resources = XMLElement(name: "resources")
-      
-      for string in strings.sorted(by: { lhs, rhs in
-          lhs.key < rhs.key
-      }) {
-          guard let value = string.value.localizations![language]?.stringUnit?.value else {
-              continue
-          }
-          
-          let element = XMLElement(name: "string")
-          let cleaned = Self.cleanupValueForAndroid(value)
-          let textNode = XMLNode(kind: .text)
-          textNode.stringValue = cleaned
-          element.addChild(textNode)
-          
-          // TODO: implement plurals
-          
-          element.addAttribute(
-            XMLNode.attribute(
-                withName: "name",
-                stringValue: Self.cleanupKeyForAndroid(string.key)
-            ) as! XMLNode
-          )
-          
-          resources.addChild(element)
+    let resources = XMLElement(name: "resources")
+    
+    for string in strings.sorted(by: { lhs, rhs in
+      lhs.key < rhs.key
+    }) {
+      guard let value = string.value.localizations![language]?.stringUnit?.value else {
+        continue
       }
       
-      return XMLDocument(rootElement: resources)
+      let element = XMLElement(name: "string")
+      let cleaned = Self.cleanupValueForAndroid(value)
+      let textNode = XMLNode(kind: .text)
+      textNode.stringValue = cleaned
+      element.addChild(textNode)
+      
+      // TODO: implement plurals
+      
+      element.addAttribute(
+        XMLNode.attribute(
+          withName: "name",
+          stringValue: Self.cleanupKeyForAndroid(string.key)
+        ) as! XMLNode
+      )
+      
+      resources.addChild(element)
+    }
+    
+    return XMLDocument(rootElement: resources)
   }
 }
