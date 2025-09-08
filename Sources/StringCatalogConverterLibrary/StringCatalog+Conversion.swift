2d1e627569807b542eea618755c22f6525b6aa5d
Wolfgang Lutz <wlut@num42.de>
2025-09-08 13:44:48 +0200

    run swift format --recursive --in-place .

--- Sources/StringCatalogConverterLibrary/StringCatalog+Conversion.swift
+++ Sources/StringCatalogConverterLibrary/StringCatalog+Conversion.swift
@@ -7,7 +7,7 @@ extension StringCatalog {
       .replacingOccurrences(of: ".", with: "_")
       .replacingOccurrences(of: "-", with: "_")
   }
-  
+
   static func cleanupValueForAndroid(_ value: String) -> String {
     value
       .replacingOccurrences(of: "\n", with: "\\n")
@@ -17,35 +17,36 @@ extension StringCatalog {
       // We assume that all formatted values have a position!
       .replacingOccurrences(of: "\\\\%([1-9])\\$@", with: "%$1\\$s", options: .regularExpression)
       .replacingOccurrences(of: "\\\\%([1-9])\\$d", with: "%$1\\$d", options: .regularExpression)
-      .replacingOccurrences(of: "\\\\%([1-9])\\$.([1-9])f", with: "%$1\\$.$2f", options: .regularExpression)
+      .replacingOccurrences(
+        of: "\\\\%([1-9])\\$.([1-9])f", with: "%$1\\$.$2f", options: .regularExpression)
   }
-  
+
   public func converted(
     to language: StringLanguage
   ) -> XMLDocument {
     let resources = XMLElement(name: "resources")
-      
-      resources.addAttribute(
-        XMLNode.attribute(
-            withName: "xmlns:tools",
-            stringValue: "http://schemas.android.com/tools"
-        ) as! XMLNode
-      )
-      
-      resources.addAttribute(
-        XMLNode.attribute(
-            withName: "tools:locale",
-            stringValue: language.rawValue
-        ) as! XMLNode
-      )
+
+    resources.addAttribute(
+      XMLNode.attribute(
+        withName: "xmlns:tools",
+        stringValue: "http://schemas.android.com/tools"
+      ) as! XMLNode
+    )
+
+    resources.addAttribute(
+      XMLNode.attribute(
+        withName: "tools:locale",
+        stringValue: language.rawValue
+      ) as! XMLNode
+    )
 
     for string in strings.sorted(by: { lhs, rhs in
       lhs.key < rhs.key
     }) {
-        guard let value = string.value.localizations![language]?.stringUnit?.value else {
-            continue
-        }
-        
+      guard let value = string.value.localizations![language]?.stringUnit?.value else {
+        continue
+      }
+
       let element = XMLElement(
         name: "string", stringValue: Self.cleanupValueForAndroid(value))
 
